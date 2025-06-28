6e34cdb70f4a70ce1f8b608f11292969f8b03262
Amr Murad <amrmurad2003@gmail.com>
2025-06-28 03:54:07 +0300

    inital commit

---
+++ src/test/java/ecommerce/shopapp/ShopAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ecommerce.shopapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
