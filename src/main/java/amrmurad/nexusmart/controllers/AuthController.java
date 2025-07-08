40fc4a5f7bda209bd97e2d96594b84ee72f03cee
Amr Murad <amrmurad2003@gmail.com>
2025-07-09 01:16:22 +0300

    auth controllers implemented

---
+++ src/main/java/amrmurad/nexusmart/controllers/AuthController.java
@@ -0,0 +1,35 @@
+package amrmurad.nexusmart.controllers;
+
+import amrmurad.nexusmart.DTOs.AuthResponse;
+import amrmurad.nexusmart.DTOs.LoginRequest;
+import amrmurad.nexusmart.DTOs.UserRegistrationRequest;
+import amrmurad.nexusmart.services.UserService;
+import com.stripe.model.tax.Registration;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+@RestControllerAdvice
+public class AuthController {
+
+    private final UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthResponse> registerUser(@Valid @RequestBody UserRegistrationRequest request){
+        AuthResponse response = userService.registerUser(request);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> authenticateUser (@Valid @RequestBody LoginRequest request){
+        AuthResponse response = userService.authenticateUser(request);
+        return ResponseEntity.ok(response);
+    }
+
+}
